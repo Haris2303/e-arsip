c841fb2b4f2d0fcf23909b886836ed4ae32ea8db
haris2303 <hrsccf102@gmail.com>
2025-06-29 14:37:09 +0900

    make bar chart

---
+++ app/Filament/Resources/IncomingMailsChartResource/Widgets/IncomingMailsChart.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Filament\Resources\IncomingMailsChartResource\Widgets;
+
+use App\Models\IncomingMail;
+use App\Models\OutgoingMail;
+use Filament\Support\RawJs;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class IncomingMailsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Surat Masuk';
+    public ?string $filter = 'today';
+
+    protected function getData(): array
+    {
+        switch ($this->filter) {
+            case 'today':
+                $start = now()->startOfDay();
+                $end = now()->endOfDay();
+                $internal = 'perHour';
+                break;
+
+            case 'week':
+                $start = now()->startOfWeek();
+                $end = now()->endOfWeek();
+                $internal = 'perDay';
+                break;
+
+            case 'month':
+                $start = now()->startOfMonth();
+                $end = now()->endOfMonth();
+                $internal = 'perWeek';
+                break;
+            default:
+                $start = now()->startOfYear();
+                $end = now()->endOfYear();
+                $internal = 'perMonth';
+        }
+
+        // Get data Incoming Mails
+        $incoming = Trend::model(IncomingMail::class)
+            ->between(start: $start, end: $end)
+            ->{$internal}()
+            ->count();
+
+        // Get data Outgoing Mails
+        $outgoing = Trend::model(OutgoingMail::class)
+            ->between(start: $start, end: $end)
+            ->{$internal}()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Surat Masuk',
+                    'data' => $incoming->map(fn(TrendValue $value) => $value->aggregate)->toArray(),
+                    'backgroundColor' => 'rgba(54, 162, 235, 0.5)', // blue
+                    'borderColor' => 'rgba(54, 162, 235, 1)',
+                    'borderWidth' => 1,
+                ],
+                [
+                    'label' => 'Surat Keluar',
+                    'data' => $outgoing->map(fn(TrendValue $value) => $value->aggregate)->toArray(),
+                    'backgroundColor' => 'rgba(255, 205, 86, 0.5)', // yellow
+                    'borderColor' => 'rgba(255, 205, 86, 1)',
+                    'borderWidth' => 1,
+                ],
+            ],
+            'labels' => $incoming->map(fn(TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'today' => 'Hari Ini',
+            'week' => 'Minggu Terakhir',
+            'month' => 'Bulan Terakhir',
+            'year' => 'Tahun Ini',
+        ];
+    }
+
+    protected function getOptions(): RawJs
+    {
+        return RawJs::make(<<<JS
+    {
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 1,
+                    callback: (value) => Number.isInteger(value) ? value : null
+                }
+            }
+        }
+    }
+    JS);
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\IncomingMailsChartResource\Widgets\IncomingMailsChart;
+use App\Filament\Resources\MailChartResource\Widgets\MailChartWidget;
+use App\Filament\Resources\MailStatsResource\Widgets\MailStatsWidget;
 use App\Http\Middleware\EnsureAdminFilamentUser;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -39,7 +42,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                IncomingMailsChart::class,
             ])
             ->middleware([
                 EncryptCookies::class,
